43adcbec5dc6fc6ecb80603fb8e18527c5e50752
Danny Jiang <dannyjiang11@outlook.com>
2022-02-27 09:01:37 +0000

    Odd Checker

---
+++ odd.js
@@ -0,0 +1,10 @@
+const isOdd = function(num) {
+  return num%2===1;
+}
+
+console.log(isOdd(3))
+console.log(isOdd(6))
+console.log(isOdd(13))
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
